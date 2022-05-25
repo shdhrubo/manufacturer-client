18eaa59376b3fcb83b1d9454dd60fd86a67aa22d
shdhrubo <shorifulhabib.iit@gmail.com>
2022-05-25 18:18:32 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
+
       <Footer></Footer>
     </div>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h4 className="text-3xl text-red-500">404</h4>
+      <p className="text-2xl">Page Not Found!</p>
+    </div>
+  );
+};
+
+export default NotFound;
