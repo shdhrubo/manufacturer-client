6c83962cd382531db30757a979575159c379971f
shdhrubo <shorifulhabib.iit@gmail.com>
2022-05-28 20:40:04 +0600

    committed

--- src/components/Home/Parts.js
+++ src/components/Home/Parts.js
@@ -2,22 +2,31 @@ import React, { useEffect, useState } from "react";
 import Part from "./Part";
 const Parts = () => {
   const [parts, setParts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetch("https://mysterious-bastion-07906.herokuapp.com/services")
       .then((res) => res.json())
       .then((data) => {
-        const sliced = data.slice(0, 3);
+        const sliced = data.slice(0, 6);
         setParts(sliced);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
   return (
     <div className="my-10">
       <h3 className="text-2xl font-bold text-blue-700">Parts We Provide</h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-10 my-8">
-        {parts.map((part) => (
-          <Part part={part} key={part.id}></Part>
-        ))}
-      </div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-10 my-8">
+          {parts.map((part) => (
+            <Part part={part} key={part.id}></Part>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Home/Reviews.js
+++ src/components/Home/Reviews.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import Loading from "../Loading/Loading";
 import Review from "./Review";
 
 const Reviews = () => {
+  const [loading, setLoading] = useState(true);
+
   const { data: reviews, isLoading } = useQuery(["reviews"], () =>
     fetch("https://mysterious-bastion-07906.herokuapp.com/reviews", {
       method: "GET",
       headers: {
         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
-    }).then((res) => res.json())
+    })
+      .then((res) => res.json())
+      .finally(() => {
+        setLoading(false);
+      })
   );
   console.log(reviews);
   if (isLoading) {
@@ -20,11 +26,15 @@ const Reviews = () => {
     <div className="mt-32 mb-16">
       <h4 className="text-2xl font-bold text-blue-700">Testimonial</h4>
       <h4 className="text-xl ">Our Customer Reviews</h4>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 mx-10">
-        {reviews?.map((review) => (
-          <Review review={review} key={review._id}></Review>
-        ))}
-      </div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 mx-10">
+          {reviews?.map((review) => (
+            <Review review={review} key={review._id}></Review>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
